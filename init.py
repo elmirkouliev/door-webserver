201d3461aeb1c31cd5b295fd0ec8ba76e2811e39
Elmir Kouliev <elmir@kouliev.com>
2017-04-15 14:36:38 -0400

    Updats

--- init.py
+++ init.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world'
+@app.route('/sensor', methods=['GET'])
+def door():
+    open = request.args.get('door')
+
+    if open is not None:
+        print(open);
+    else:
+        return 'Missing parameters!', 400
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Not found!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
